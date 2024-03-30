158c76014bbc6df97460fc82ded4b4207730dbf3
RafaelFernandes12 <rfsilva786@gmail.com>
2024-03-30 17:08:09 -0300

    starting the responsive site

--- src/app/(app)/SobreNos/page.tsx
+++ src/app/(app)/SobreNos/page.tsx
@@ -4,14 +4,15 @@ export default function SobreNos() {
     <div className="flex items-center flex-col max-w-[860px] m-auto">
       <h1 className="text-3xl font-medium m-4">Quem Somos?</h1>
       <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit 
-            minus consectetur accusantium nisi a 
-            exercitationem rem ducimus iste debitis natus error obcaecati magnam 
-            veniam ut et, quasi perspiciatis at? Inventore.
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam cumque vero veritatis acc
-            usantium. Quo molestiae hic numquam voluptas rerum quidem aut voluptat
-            e alias provident quas? Et alias consectetur voluptatibus doloremque!
+        O projeto CPSI-Terceira Idade surgiu como um projeto de extensão no IFRN, no qual procurava facilitar, pelo meio
+        da criação de um sistema para internet, o cadastro dos alunos no centro de idosos, o qual era feito no papel até o momento.
       </p>
+      <p>
+        O site foi desenvolvido pelos estudantes Rafael Fernandes e Matheus Henrique, ambos técnicos em informática para internet
+        pelo IFRN, e graduantos em Tecnologia da Informação pela UFRN.
+      </p>
+
+      
     </div>
   );
 }
